c7dba192592b34712683a75db7287a979c25fd07
parsa <parsa.asgari3264@gmail.com>
2022-06-04 06:28:18 +0000

    first commit

---
+++ src/settings.php
@@ -0,0 +1,14 @@
+<?php
+    //Database settings
+    $dbhost = "34.121.152.190"; //Database host
+    $dbuser = "parsa"; //Database username
+    $dbpassword = "]l~BtDsf>}q8h4Zr"; //Database Password. Leave blank for no password!
+    $dbname = "php_guestbook"; //Database Name;
+
+    //General Settings
+    $sitename = "Guestbook"; //Website Name;
+
+
+    //Dont edit this line!
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+?>
