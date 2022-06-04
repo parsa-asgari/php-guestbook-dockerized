a4b0c0a90a061a8768e0a8c61207c83b2a1bfdd9
parsa <parsa.asgari3264@gmail.com>
2022-06-04 19:52:22 +0000

    hopefully, the VPC thing might work.

--- src/settings.php
+++ src/settings.php
@@ -1,7 +1,7 @@
 <?php
     //Database settings
-//    $dbhost = "34.121.152.190"; //Database host
-    $dbhost = "172.16.1.50";
+    $dbhost = "10.20.96.3"; //Database host
+//    $dbhost = "172.16.1.50"; //local database
     $dbuser = "parsa"; //Database username
     $dbpassword = "]l~BtDsf>}q8h4Zr"; //Database Password. Leave blank for no password!
     $dbname = "php_guestbook"; //Database Name;
